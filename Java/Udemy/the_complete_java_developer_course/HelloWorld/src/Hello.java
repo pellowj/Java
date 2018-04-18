6c1db344336c5f48d018382541e6954e38c81762
John Pellow <pellowj@aol.com>
2018-04-17 20:10:40 -0500

    Add files via upload

--- Java/Udemy/the_complete_java_developer_course/HelloWorld/src/Hello.java
+++ Java/Udemy/the_complete_java_developer_course/HelloWorld/src/Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, John!");
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int minusThousand = 1000 - myTotal;
+        System.out.println(minusThousand);
     }
 }
