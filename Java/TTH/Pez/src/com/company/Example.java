c61891687efde54661970d79138fcb8258004a3c
John Pellow <pellowj@aol.com>
2018-04-24 21:54:39 -0500

    Add files via upload

--- Java/TTH/Pez/src/com/company/Example.java
+++ Java/TTH/Pez/src/com/company/Example.java
@@ -4,7 +4,20 @@ public class Example {
 
     public static void main(String[] args) {
         System.out.println("We are making a new PEZ Dispenser");
+        System.out.printf("FUN FACT: There are %d PEZ allowed in every dispenser %n",
+                            PezDispenser.MAX_PEZ);
         PezDispenser dispenser = new PezDispenser("Yoda");
         System.out.printf("The dispenser is %s %n", dispenser.getCharacterName());
+
+        if (dispenser.isEmpty()) {
+            System.out.println("Dispenser is Empty");
+        }
+
+        System.out.println("Filling the dispenser with delicious PEZ...");
+        dispenser.fill();
+
+        if (!dispenser.isEmpty()) {
+            System.out.println("Dispenser is full");
+        }
     }
 }
--- Java/TTH/Pez/src/com/company/PezDispenser.java
+++ Java/TTH/Pez/src/com/company/PezDispenser.java
@@ -1,11 +1,21 @@
 package com.company;
 
 class PezDispenser {
-    final public int MAX_PEZ = 12;
+    final public static int MAX_PEZ = 12;
     final private String characterName;
+    private int pezCount;
 
     public PezDispenser(String characterName) {
         this.characterName = characterName;
+        pezCount = 0;
+    }
+
+    public void fill() {
+        pezCount = MAX_PEZ;
+    }
+
+    public boolean isEmpty() {
+        return pezCount == 0;
     }
 
     public String getCharacterName() {
