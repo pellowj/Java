f22e7fff7e8ca5b03853768fbc0b0fb17eea484c
John Pellow <pellowj@aol.com>
2018-04-25 21:08:36 -0500

    Add files via upload

---
+++ Java/TTH/Hangman/src/com/company/Game.java
@@ -0,0 +1,9 @@
+package com.company;
+
+class Game {
+    private String answer;
+
+    public Game(String answer){
+        this.answer = answer;
+    }
+}
---
+++ Java/TTH/Hangman/src/com/company/Hangman.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Hangman {
+
+    public static void main(String[] args) {
+        Game game = new Game("treehouse");
+    }
+}
